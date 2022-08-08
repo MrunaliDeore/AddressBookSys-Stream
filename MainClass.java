26007f74d93ad2a36739a51fa8d1e862ff1c0d47
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-08-08 10:55:27 +0530

    Added : mainclass file - welcome msg here

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+package Stream;
+
+public class MainClass {
+	
+	public static void main(String[] args){
+		System.out.println("Assignments no. 24\n");
+		System.out.println("Address Book System by using STREAM");
+		
+	}
+
+}
